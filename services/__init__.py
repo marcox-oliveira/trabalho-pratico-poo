ef6a697e9aa94ff7b88c1fe03a3be0ac408d32d8
Marcos Oliveira <marcosilva.mo91@gmail.com>
2026-06-30 12:07:40 -0300

    Create __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,5 @@
+from .usuario_servico import UsuarioServico
+from .musica_servico import MusicaServico
+from .playlist_servico import PlaylistServico
+
+__all__ = ["UsuarioServico", "MusicaServico", "PlaylistServico"]
