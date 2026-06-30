ee9edf1d628f9b393e3e9f487ef9cb099390b6a6
Marcos Oliveira <marcosilva.mo91@gmail.com>
2026-06-30 11:54:26 -0300

    Create __init__.py

---
+++ repositories/__init__.py
@@ -0,0 +1,10 @@
+from .usuario_repo import UsuarioRepositorio
+from .artista_repo import ArtistaRepositorio
+from .album_repo import AlbumRepositorio
+from .musica_repo import MusicaRepositorio
+from .playlist_repo import PlaylistRepositorio
+
+__all__ = [
+    "UsuarioRepositorio", "ArtistaRepositorio",
+    "AlbumRepositorio", "MusicaRepositorio", "PlaylistRepositorio",
+]
